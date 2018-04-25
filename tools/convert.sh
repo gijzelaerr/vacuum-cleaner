edf1e16f38e8410c70522b690092f426fe141435
Gijs Molenaar <gijs@pythonic.nl>
2018-04-25 10:40:04 +0200

    add convert bash script

---
+++ tools/convert.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -ve
+
+INPUT=$1
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TARGET=${HERE}/../datasets/spiel/
+
+if [[ $# -eq 0 ]] ; then
+    echo 'usage: $0 <fits folder>'
+    exit 1
+fi
+
+mkdir -p $TARGET/train
+mkdir -p $TARGET/test
+mkdir -p $TARGET/val
+
+
+for i in $(seq 0 399); do
+    echo train/$i
+    python $HERE/fits_merge.py \
+        $INPUT/1-skymodel.fits \
+        $INPUT/1-wsclean-dirty.fits \
+        $TARGET/train/$i
+done
+
+for i in $(seq 400 499); do
+    echo test/$i
+    python $HERE/fits_merge.py \
+        $INPUT/1-skymodel.fits \
+        $INPUT/1-wsclean-dirty.fits \
+        $TARGET/test/$i
+done
+
+for i in $(seq 500 599); do
+    echo val/$i
+    python $HERE/fits_merge.py \
+        $INPUT/1-skymodel.fits \
+        $INPUT/1-wsclean-dirty.fits \
+        $TARGET/val/$i
+done
+
