0afb78ea9af246f4c2306d1f693b67fa8ea51fcf
Gijs Molenaar <gijs@pythonic.nl>
2018-10-29 14:55:47 +0100

    add header copy thingy

---
+++ tools/fits_copy_header.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+from astropy.io import fits
+
+
+def copy_header(source_path, target_path):
+    source = fits.open(source_path, mode='readonly')
+    target = fits.open(target_path, mode='update')
+
+    target[0].header = source[0].header
+    target.flush()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("\n  usage: {} <source.fits> <target.fits>\n".format(sys.argv[0]))
+        sys.exit(1)
+
+    source_path = sys.argv[1]
+    target_path = sys.argv[2]
+    copy_header(source_path, target_path)
